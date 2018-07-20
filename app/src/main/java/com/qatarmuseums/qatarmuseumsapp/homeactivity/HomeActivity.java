174959e321dbdba4ba2fefaf6f1c3c6bb0ac14d8
Sanoop c <sanoopchullian22@gmail.com>
2018-07-21 00:45:58 +0530

    Home Page UI

--- app/src/main/java/com/qatarmuseums/qatarmuseumsapp/homeactivity/HomeActivity.java
+++ app/src/main/java/com/qatarmuseums/qatarmuseumsapp/homeactivity/HomeActivity.java
@@ -1,27 +1,36 @@
 package com.qatarmuseums.qatarmuseumsapp.homeactivity;
 
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.qatarmuseums.qatarmuseumsapp.R;
 import com.qatarmuseums.qatarmuseumsapp.baseactivity.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends BaseActivity {
-    LinearLayout diningNavigation, giftShopNavigation, culturePassNavigation, moreNavigation;
+    RelativeLayout diningNavigation, giftShopNavigation, culturePassNavigation, moreNavigation;
     Animation zoomOutAnimation;
+    private RecyclerView recyclerView;
+    private HomeListAdapter mAdapter;
+    private List<HomeList> movieList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        diningNavigation = (LinearLayout) findViewById(R.id.dining_layout);
-        giftShopNavigation = (LinearLayout) findViewById(R.id.gift_shop_layout);
-        culturePassNavigation = (LinearLayout) findViewById(R.id.culture_pass_layout);
-        moreNavigation = (LinearLayout) findViewById(R.id.more_layout);
+        diningNavigation = (RelativeLayout) findViewById(R.id.dining_layout);
+        giftShopNavigation = (RelativeLayout) findViewById(R.id.gift_shop_layout);
+        culturePassNavigation = (RelativeLayout) findViewById(R.id.culture_pass_layout);
+        moreNavigation = (RelativeLayout) findViewById(R.id.more_layout);
 
         zoomOutAnimation = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.zoom_out);
@@ -49,6 +58,15 @@ public class HomeActivity extends BaseActivity {
                 handlingDrawer();
             }
         });
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mAdapter = new HomeListAdapter(movieList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareMovieData();
 
         diningNavigation.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -94,5 +112,29 @@ public class HomeActivity extends BaseActivity {
                 return false;
             }
         });
+    }
+
+    private void prepareMovieData() {
+        HomeList movie = new HomeList("Mathaf: Arab Museum of Modern Art", "61",
+                "http://www.qm.org.qa/sites/default/files/museum_of_islamic_art.png",
+                2, true);
+        movieList.add(movie);
+        movie = new HomeList("Museum of Islamic Art (MIA)", "61",
+                "http://www.qm.org.qa/sites/default/files/museum_of_islamic_art.png",
+                2, true);
+        movieList.add(movie);
+        movie = new HomeList("National Museum of Qatar", "61",
+                "http://www.qm.org.qa/sites/default/files/museum_of_islamic_art.png",
+                2, true);
+        movieList.add(movie);
+        movie = new HomeList("3-2-1 Qatar Olympic and Sports Museum", "61",
+                "http://www.qm.org.qa/sites/default/files/museum_of_islamic_art.png",
+                2, true);
+        movieList.add(movie);
+        movie = new HomeList("Garage Gallery, Fire Station", "61",
+                "http://www.qm.org.qa/sites/default/files/museum_of_islamic_art.png",
+                2, false);
+        movieList.add(movie);
+        mAdapter.notifyDataSetChanged();
     }
 }
