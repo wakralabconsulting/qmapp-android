ce02a3accffbc242db5ff1fc9260cd03dbe2b008
ExalturePC <jothys@exalture.com>
2018-07-25 17:24:00 +0530

    calendar ui completed

--- qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/Calendar/CalendarAdapter.java
+++ qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/Calendar/CalendarAdapter.java
@@ -1,18 +1,21 @@
 package com.qatarmuseums.qatarmuseumsapp.Calendar;
 
-import android.Manifest;
+import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,44 +53,17 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.Calend
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CalendarAdapter.CalendarAdapterViewHolder holder, int position) {
-        SimpleDateFormat yyyyMMdd = new SimpleDateFormat("yyyyMMdd");
-        Calendar dt = Calendar.getInstance();
+    public void onBindViewHolder(@NonNull final CalendarAdapter.CalendarAdapterViewHolder holder, int position) {
 
-// Where untilDate is a date instance of your choice, for example 30/01/2012
-//                dt.setTime(untilDate);
-
-        // If you want the event until 30/01/2012, you must add one day from our day because
-        // UNTIL in RRule sets events before the last day
-        dt.add(Calendar.DATE, 1);
-        final String dtUntill = yyyyMMdd.format(dt.getTime());
+        if (position % 2 == 1) {
+            holder.layoutHolder.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        } else {
+            holder.layoutHolder.setBackgroundColor(Color.parseColor("#FFf2f2f2"));
+        }
         holder.viewDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ContentResolver contentResolver = context.getContentResolver();
-                ContentValues cv = new ContentValues();
-                cv.put(CalendarContract.Events.TITLE, "Birthday Celebration");
-                cv.put(CalendarContract.Events.DESCRIPTION, "DESCRIPTION OF THE EVENT");
-                cv.put(CalendarContract.Events.DTSTART, Calendar.getInstance().getTimeInMillis());
-                cv.put(CalendarContract.Events.DTEND, Calendar.getInstance().getTimeInMillis() + 60 * 60 * 1000);
-                // Default calendar
-                cv.put(CalendarContract.Events.CALENDAR_ID, 1);
-                TimeZone timeZone = TimeZone.getDefault();
-                cv.put(CalendarContract.Events.EVENT_TIMEZONE, Calendar.getInstance().getTimeZone().getID());
-
-//
-//                cv.put(CalendarContract.Events.RRULE, "FREQ=DAILY;UNTIL="
-//                        + dtUntill);
-// Set Period for 1 Hour
-//                cv.put(CalendarContract.Events.DURATION, "+P1H");
-//
-//                cv.put(CalendarContract.Events.HAS_ALARM, 1);
-
-// Insert event to calendar
-                Uri uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, cv);
-                Toast.makeText(context,"entered",Toast.LENGTH_SHORT).show();
-
-
+                showDialog(holder.eventDetails.getText().toString());
             }
         });
 
@@ -101,6 +77,7 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.Calend
     public class CalendarAdapterViewHolder extends RecyclerView.ViewHolder {
 
         public TextView eventTitle, eventSubTitle, eventDetails, eventTiming, viewDetails;
+        public LinearLayout layoutHolder;
 
         public CalendarAdapterViewHolder(View itemView) {
             super(itemView);
@@ -109,7 +86,66 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.Calend
             eventDetails = (TextView) itemView.findViewById(R.id.event_detail_text);
             eventTiming = (TextView) itemView.findViewById(R.id.event_timing);
             viewDetails = (TextView) itemView.findViewById(R.id.event_view_details);
+            layoutHolder=(LinearLayout)itemView.findViewById(R.id.layout_holder);
 
         }
+    }
+
+
+    protected void showDialog(final String details) {
+
+        final Dialog dialog = new Dialog(context, R.style.DialogTheme);
+        dialog.setCancelable(true);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.common_popup, null);
+
+        dialog.setContentView(view);
+        ImageView closeBtn = (ImageView) view.findViewById(R.id.close_dialog);
+        Button changeLanguageBtn = (Button) view.findViewById(R.id.doneBtn);
+        TextView dialogTitle = (TextView) view.findViewById(R.id.dialog_tittle);
+        TextView dialogContent = (TextView) view.findViewById(R.id.dialog_content);
+        dialogTitle.setText(context.getResources().getString(R.string.calendar_dialog_title));
+        changeLanguageBtn.setText(context.getResources().getString(R.string.add_to_calendar));
+        dialogContent.setText(details);
+
+        changeLanguageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Do something
+                addToCalendar(details);
+                dialog.dismiss();
+
+            }
+        });
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Do something
+                dialog.dismiss();
+
+            }
+        });
+        dialog.show();
+    }
+
+    private void addToCalendar(String details) {
+
+        ContentResolver contentResolver = context.getContentResolver();
+        ContentValues cv = new ContentValues();
+        cv.put(CalendarContract.Events.TITLE, "Title of the event");
+        cv.put(CalendarContract.Events.DESCRIPTION, details);
+        cv.put(CalendarContract.Events.DTSTART, Calendar.getInstance().getTimeInMillis());
+        cv.put(CalendarContract.Events.DTEND, Calendar.getInstance().getTimeInMillis() + 60 * 60 * 1000);
+        // Default calendar
+        cv.put(CalendarContract.Events.CALENDAR_ID, 1);
+        TimeZone timeZone = TimeZone.getDefault();
+        cv.put(CalendarContract.Events.EVENT_TIMEZONE, Calendar.getInstance().getTimeZone().getID());
+
+        // Insert event to calendar
+        Uri uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, cv);
+        Toast.makeText(context, "entered", Toast.LENGTH_SHORT).show();
+
     }
 }
