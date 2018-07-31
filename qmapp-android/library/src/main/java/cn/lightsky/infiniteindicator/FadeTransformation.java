3a3fc6c6dd9452cb7cc669b15afeb0def25171d1
Dileep <dileepkaramel123@gmail.com>
2018-07-31 10:15:06 +0530

    unwanted code removed

--- qmapp-android/library/src/main/java/cn/lightsky/infiniteindicator/FadeTransformation.java
+++ qmapp-android/library/src/main/java/cn/lightsky/infiniteindicator/FadeTransformation.java
@@ -8,23 +8,7 @@ public class FadeTransformation implements ViewPager.PageTransformer {
     private static final float MIN_ALPHA = 0.3f;
     @Override
     public void transformPage(View view, float position) {
-//        if (position <-1){  // [-Infinity,-1)
-//            // This page is way off-screen to the left.
-//            page.setAlpha(0);
-//
-//        }
-//        else if (position <=1){ // [-1,1]
-//
-//            page.setScaleX(Math.max(MIN_SCALE,1-Math.abs(position)));
-//            page.setScaleY(Math.max(MIN_SCALE,1-Math.abs(position)));
-//            page.setAlpha(Math.max(MIN_ALPHA,1-Math.abs(position)));
-//
-//        }
-//        else {  // (1,+Infinity]
-//            // This page is way off-screen to the right.
-//            page.setAlpha(0);
-//
-//        }
+
         if(position <= -1.0F || position >= 1.0F) {
             view.setTranslationX(view.getWidth() * position);
             view.setAlpha(0.0F);
