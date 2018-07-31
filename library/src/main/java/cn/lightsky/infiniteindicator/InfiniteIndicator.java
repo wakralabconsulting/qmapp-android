5401288181a6ba64643f968b300a221e14f6d62f
Dileep <dileepkaramel123@gmail.com>
2018-07-31 10:15:06 +0530

    unwanted code removed

--- library/src/main/java/cn/lightsky/infiniteindicator/InfiniteIndicator.java
+++ library/src/main/java/cn/lightsky/infiniteindicator/InfiniteIndicator.java
@@ -77,21 +77,6 @@ public class InfiniteIndicator extends RelativeLayout implements
                 ,configuration.getOnPageClickListener());
         mRecyleAdapter.setDataChangeListener(this);
         mViewPager.setAdapter(mRecyleAdapter);
-//        mViewPager.setPageTransformer(false,new ViewPager.PageTransformer() {
-//            @Override
-//            public void transformPage(View view, float position) {
-////                view.setTranslationX(view.getWidth() * -position);
-////
-////                if(position <= -1.0F || position >= 1.0F) {
-////                    view.setAlpha(0.0F);
-////                } else if( position == 0.0F ) {
-////                    view.setAlpha(1.0F);
-////                } else {
-////                    // position is between -1.0F & 0.0F OR 0.0F & 1.0F
-////                    view.setAlpha(1.0F - Math.abs(position));
-////                }
-//            }
-//        });
         mViewPager.addOnPageChangeListener(this);
         mRecyleAdapter.setIsLoop(configuration.isLoop());
         mRecyleAdapter.setImageLoader(configuration.getImageLoader());
