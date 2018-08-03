1bb35eba80a34df9f9fd4112f7966f1d5d6447e4
Dileep <dileepkaramel123@gmail.com>
2018-08-03 15:35:43 +0530

    home API,exhibition API, Heritage API, partially completed, need check after api problem solved

--- app/src/main/java/com/qatarmuseums/qatarmuseumsapp/apicall/APIInterface.java
+++ app/src/main/java/com/qatarmuseums/qatarmuseumsapp/apicall/APIInterface.java
@@ -12,7 +12,8 @@ import retrofit2.http.Path;
 public interface APIInterface {
     @GET("{language}/mobile_api/gethomeList.json")
     Call<ArrayList<HomeList>> getHomepageDetails(@Path("language") String language);
-    @GET("{language}/mobile_api/Exhibition_List_Page.json")
-    Call<ArrayList<CommonModel>> getExhibitionpageList(@Path("language") String language);
+    @GET("{language}/mobile_api/{pageName}")
+    Call<ArrayList<CommonModel>> getCommonpageList(@Path("language") String language,
+                                                   @Path("pageName") String pageName );
 
 }
