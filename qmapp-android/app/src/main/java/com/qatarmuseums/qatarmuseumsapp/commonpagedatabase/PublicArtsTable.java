57105f3c1ad1231b8d0dab36fa175d69ad081fd6
ExalturePC <jothys@exalture.com>
2018-08-09 07:09:50 +0530

    public atrs database

---
+++ qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/commonpagedatabase/PublicArtsTable.java
@@ -0,0 +1,141 @@
+package com.qatarmuseums.qatarmuseumsapp.commonpagedatabase;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import com.qatarmuseums.qatarmuseumsapp.home.HomePageTable;
+
+/**
+ * Created by MoongedePC on 08-Aug-18.
+ */
+
+@Entity(tableName = "publicartstable")
+public class PublicArtsTable {
+    @NonNull
+    @PrimaryKey(autoGenerate = false)
+    private long public_arts_id;
+    @ColumnInfo()
+    private String public_arts_name;
+    @ColumnInfo()
+    private String tourguide_available;
+    @ColumnInfo()
+    private String public_arts_image;
+    @ColumnInfo()
+    private String public_arts_arabic_name;
+    @ColumnInfo()
+    private String longitude;
+    @ColumnInfo()
+    private String latitude;
+    @ColumnInfo()
+    private String teaser_image;
+    @ColumnInfo()
+    private String description;
+    @ColumnInfo()
+    private String short_description;
+
+    public PublicArtsTable() {
+    }
+
+    @NonNull
+    public long getPublic_arts_id() {
+        return public_arts_id;
+    }
+
+    public void setPublic_arts_id(@NonNull long public_arts_id) {
+        this.public_arts_id = public_arts_id;
+    }
+
+    public String getPublic_arts_name() {
+        return public_arts_name;
+    }
+
+    public void setPublic_arts_name(String public_arts_name) {
+        this.public_arts_name = public_arts_name;
+    }
+
+    public String getTourguide_available() {
+        return tourguide_available;
+    }
+
+    public void setTourguide_available(String tourguide_available) {
+        this.tourguide_available = tourguide_available;
+    }
+
+    public String getPublic_arts_image() {
+        return public_arts_image;
+    }
+
+    public void setPublic_arts_image(String public_arts_image) {
+        this.public_arts_image = public_arts_image;
+    }
+
+    public String getPublic_arts_arabic_name() {
+        return public_arts_arabic_name;
+    }
+
+    public void setPublic_arts_arabic_name(String public_arts_arabic_name) {
+        this.public_arts_arabic_name = public_arts_arabic_name;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getTeaser_image() {
+        return teaser_image;
+    }
+
+    public void setTeaser_image(String teaser_image) {
+        this.teaser_image = teaser_image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getShort_description() {
+        return short_description;
+    }
+
+    public void setShort_description(String short_description) {
+        this.short_description = short_description;
+    }
+
+    @Override
+    public String toString() {
+        return "PublicArtsTable{" +
+                "name=" + public_arts_name +
+                ", qatarmuseum_id='" + public_arts_id
+                + '\'' + ",image='"+public_arts_image+'\''+
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof PublicArtsTable)) return false;
+
+        PublicArtsTable publicArtsTable = (PublicArtsTable) obj;
+
+        if (public_arts_id != publicArtsTable.public_arts_id) return false;
+        return public_arts_name != null ? publicArtsTable.equals(publicArtsTable.public_arts_name) : publicArtsTable.public_arts_name == null;
+    }
+}
