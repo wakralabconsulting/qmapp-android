d29e73bbcf6bbec3fabb7d2110e87e3ab61cfeac
Dileep <dileepkaramel123@gmail.com>
2018-08-20 14:29:14 +0530

    firebase analytics default setup completed

--- app/src/main/java/com/qatarmuseums/qatarmuseumsapp/home/HomeActivity.java
+++ app/src/main/java/com/qatarmuseums/qatarmuseumsapp/home/HomeActivity.java
@@ -18,8 +18,6 @@ import android.view.animation.AnimationUtils;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import com.crashlytics.android.Crashlytics;
 import com.qatarmuseums.qatarmuseumsapp.QMDatabase;
 import com.qatarmuseums.qatarmuseumsapp.R;
 import com.qatarmuseums.qatarmuseumsapp.apicall.APIClient;
