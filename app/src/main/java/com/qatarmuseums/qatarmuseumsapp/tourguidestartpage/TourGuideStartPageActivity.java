1465e8f93b4a5cf8bd93fa3711a5f65b09c93563
Dileep <dileepkaramel123@gmail.com>
2018-09-05 13:38:23 +0530

    start tour btn animation,popup radious thickness, level divider thickness reduced

--- app/src/main/java/com/qatarmuseums/qatarmuseumsapp/tourguidestartpage/TourGuideStartPageActivity.java
+++ app/src/main/java/com/qatarmuseums/qatarmuseumsapp/tourguidestartpage/TourGuideStartPageActivity.java
@@ -3,7 +3,10 @@ package com.qatarmuseums.qatarmuseumsapp.tourguidestartpage;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -18,6 +21,7 @@ public class TourGuideStartPageActivity extends AppCompatActivity {
     ImageView playButton;
     TextView museumTitle, museumDesc;
     Button startBtn;
+    private Animation zoomOutAnimation;
 
 
     @Override
@@ -29,14 +33,43 @@ public class TourGuideStartPageActivity extends AppCompatActivity {
         museumTitle = (TextView) findViewById(R.id.museum_tittle);
         museumDesc = (TextView) findViewById(R.id.museum_desc);
         startBtn = (Button) findViewById(R.id.start_btn);
+        zoomOutAnimation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.zoom_out);
+        zoomOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent i = new Intent(TourGuideStartPageActivity.this, FloorMapActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
         mainLayout.setBackground(this.getDrawable(R.drawable.museum_of_islamic));
         museumTitle.setText(getString(R.string.mia_tour_guide));
         museumDesc.setText(getString(R.string.tourguide_title_desc));
-        startBtn.setOnClickListener(new View.OnClickListener() {
+//        startBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+        startBtn.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(TourGuideStartPageActivity.this, FloorMapActivity.class);
-                startActivity(i);
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startBtn.startAnimation(zoomOutAnimation);
+                        break;
+                }
+                return false;
             }
         });
 
