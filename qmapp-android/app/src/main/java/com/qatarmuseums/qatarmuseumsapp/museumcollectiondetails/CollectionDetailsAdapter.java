9c6e2766b9f9135f9f38a310bb81e4dfd7fbb6fe
Dileep <dileepkaramel123@gmail.com>
2018-09-21 15:21:14 +0530

    modification in collection list API complete

--- qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/museumcollectiondetails/CollectionDetailsAdapter.java
+++ qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/museumcollectiondetails/CollectionDetailsAdapter.java
@@ -30,18 +30,18 @@ public class CollectionDetailsAdapter extends  RecyclerView.Adapter<CollectionDe
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
       CollectionDetailsList cdl = collectionDetailsList.get(position);
       holder.mainTitle.setText(cdl.getMainTitle());
-        holder.subTitle.setText(cdl.getAbout());
-        holder.firstDescription.setText(cdl.getFirstDescription());
-        holder.secondDescription.setText(cdl.getSecondDescription());
-        holder.thirddDescription.setText(cdl.getThirdDescription());
+//        holder.subTitle.setText(cdl.getAbout());
+//        holder.firstDescription.setText(cdl.getFirstDescription());
+        holder.secondDescription.setText(cdl.getFirstDescription());
+//        holder.thirddDescription.setText(cdl.getThirdDescription());
         GlideApp.with(mContext)
                 .load(cdl.getImage1())
                 .placeholder(R.drawable.placeholder)
                 .into(holder.image1);
-        GlideApp.with(mContext)
-                .load(cdl.getImage2())
-                .placeholder(R.drawable.placeholder)
-                .into(holder.image2);
+//        GlideApp.with(mContext)
+//                .load(cdl.getImage2())
+//                .placeholder(R.drawable.placeholder)
+//                .into(holder.image2);
 
     }
 
@@ -51,18 +51,18 @@ public class CollectionDetailsAdapter extends  RecyclerView.Adapter<CollectionDe
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView mainTitle, subTitle, firstDescription,
-                secondDescription,thirddDescription;
-        public ImageView image1,image2;
+        public TextView mainTitle,/* subTitle, firstDescription,*/
+                secondDescription/*,thirddDescription*/;
+        public ImageView image1/*,image2*/;
         public MyViewHolder(View view) {
             super(view);
             image1 = (ImageView) view.findViewById(R.id.collection_image1);
-            image2 = (ImageView) view.findViewById(R.id.collection_image2);
+            //image2 = (ImageView) view.findViewById(R.id.collection_image2);
             mainTitle = (TextView) view.findViewById(R.id.main_title);
-            subTitle = (TextView) view.findViewById(R.id.sub_title);
-            firstDescription = (TextView) view.findViewById(R.id.first_description);
+           // subTitle = (TextView) view.findViewById(R.id.sub_title);
+            //firstDescription = (TextView) view.findViewById(R.id.first_description);
             secondDescription = (TextView) view.findViewById(R.id.second_description);
-            thirddDescription = (TextView) view.findViewById(R.id.third_description);
+           // thirddDescription = (TextView) view.findViewById(R.id.third_description);
         }
     }
 
