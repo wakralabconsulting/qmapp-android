e6d622a2fbf106b56d5ed047b549b87e39fa82f7
Dileep <dileepkaramel123@gmail.com>
2018-09-27 00:16:13 +0530

    storing selfguide start page data to DB completed

---
+++ qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/tourguidestartpage/TourGuideStartPageDao.java
@@ -0,0 +1,48 @@
+package com.qatarmuseums.qatarmuseumsapp.tourguidestartpage;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+@Dao
+public interface TourGuideStartPageDao {
+
+    @Query("SELECT * FROM tourguidestartpageenglish")
+    TourGuideStartPageEnglish getAllTourGuideStartDataEnglish();
+
+    @Query("SELECT * FROM tourguidestartpagearabic")
+    TourGuideStartPageArabic getAllTourGuideStartDataArabic();
+
+    @Query("SELECT COUNT(museum_entity) FROM tourguidestartpageenglish")
+    int getNumberOfRowsEnglish();
+
+    @Query("SELECT COUNT(museum_entity) FROM tourguidestartpagearabic")
+    int getNumberOfRowsArabic();
+
+    @Query("SELECT COUNT(museum_entity) FROM tourguidestartpageenglish WHERE museum_entity = :mEntityFromAPI")
+    int checkEnglishIdExist(String mEntityFromAPI);
+
+    @Query("SELECT COUNT(museum_entity) FROM tourguidestartpagearabic WHERE museum_entity = :mEntityFromAPI")
+    int checkArabicIdExist(String mEntityFromAPI);
+
+    @Query("SELECT * FROM tourguidestartpageenglish WHERE museum_entity = :mEntityFromAPI")
+    TourGuideStartPageEnglish getTourGuideStartPageDataEnglish(String mEntityFromAPI);
+
+    @Query("SELECT * FROM museumabouttablearabic WHERE museum_id = :mEntityFromAPI")
+    TourGuideStartPageArabic getTourGuideStartPageDataArabic(int mEntityFromAPI);
+
+    @Query("UPDATE tourguidestartpageenglish SET title = :museumtitleFromApi," +
+            "description = :tourguidedescriptionFromApi WHERE museum_entity=:mEntityFromApi")
+    void updateMuseumAboutDataEnglish( String museumtitleFromApi, String tourguidedescriptionFromApi,
+                                       String mEntityFromApi);
+    @Query("UPDATE tourguidestartpagearabic SET title = :museumtitleFromApi," +
+            "description = :tourguidedescriptionFromApi WHERE museum_entity=:mEntityFromApi")
+    void updateMuseumAboutDataArabic( String museumtitleFromApi, String tourguidedescriptionFromApi,
+                                       String mEntityFromApi);
+
+    @Insert
+    void insert(TourGuideStartPageEnglish tourGuideStartPageEnglish);
+
+    @Insert
+    void insert(TourGuideStartPageArabic tourGuideStartPageArabic);
+}
