955df0a1a1f2a78072d08d36deb9d4164ea8cdf3
Sanoop c <sanoopchullian22@gmail.com>
2018-10-10 22:22:19 +0530

    Object preview details page API

--- qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/objectpreview/PagerAdapter.java
+++ qmapp-android/app/src/main/java/com/qatarmuseums/qatarmuseumsapp/objectpreview/PagerAdapter.java
@@ -4,17 +4,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.qatarmuseums.qatarmuseumsapp.floormap.ArtifactDetails;
+
 import java.util.ArrayList;
 
 class PagerAdapter extends FragmentPagerAdapter {
     int count;
-    ArrayList<ObjectPreviewModel> objectPreviewModels =new ArrayList<>();
+    ArrayList<ArtifactDetails> objectPreviewModels = new ArrayList<>();
     PageFragment pageFragment;
 
-    public PagerAdapter(FragmentManager fm, int count, ArrayList<ObjectPreviewModel> objectPreviewModels) {
+    public PagerAdapter(FragmentManager fm, int count, ArrayList<ArtifactDetails> objectPreviewModels) {
         super(fm);
-        this.count =count;
-        this.objectPreviewModels =objectPreviewModels;
+        this.count = count;
+        this.objectPreviewModels = objectPreviewModels;
         pageFragment = new PageFragment();
     }
 
@@ -25,11 +27,13 @@ class PagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return pageFragment.newInstance(position , position == getCount(),
-                objectPreviewModels.get(position).getMainTitle(),objectPreviewModels.get(position).getAccessionNumber(),
-                objectPreviewModels.get(position).getImage(),objectPreviewModels.get(position).getProduction(),
-                objectPreviewModels.get(position).getProductionDates(),objectPreviewModels.get(position).getPeriodStyle(),
-                objectPreviewModels.get(position).getTechniqueandMaterials(),objectPreviewModels.get(position).getDimensions());
+        return pageFragment.newInstance(position, position == getCount(),
+                objectPreviewModels.get(position).getMainTitle(), objectPreviewModels.get(position).getAccessionNumber(),
+                objectPreviewModels.get(position).getImage(), objectPreviewModels.get(position).getProduction(),
+                objectPreviewModels.get(position).getProductionDates(), objectPreviewModels.get(position).getPeriodStyle(),
+                objectPreviewModels.get(position).getTechniqueMaterials(), objectPreviewModels.get(position).getDimensions(),
+                objectPreviewModels.get(position).getCuratorialDescription(), objectPreviewModels.get(position).getObjectHistory(),
+                objectPreviewModels.get(position).getObjectENGSummary(), objectPreviewModels.get(position).getImages());
     }
 
 }
