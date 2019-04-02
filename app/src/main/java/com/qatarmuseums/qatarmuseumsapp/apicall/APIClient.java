c5e4d36f32cc4fb1a23b9f51e51597fecd82da83
Sanoop c <sanoopchullian22@gmail.com>
2019-04-02 16:11:14 +0530

    SSL Pinning implemented

--- app/src/main/java/com/qatarmuseums/qatarmuseumsapp/apicall/APIClient.java
+++ app/src/main/java/com/qatarmuseums/qatarmuseumsapp/apicall/APIClient.java
@@ -1,26 +1,32 @@
 package com.qatarmuseums.qatarmuseumsapp.apicall;
 
+import okhttp3.CertificatePinner;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
-    private static Retrofit retrofit = null;
     public static String apiBaseUrl = "https://www.qm.org.qa/";
-    private static OkHttpClient client;
 
     public static Retrofit getClient() {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
-        retrofit = new Retrofit.Builder()
+        // SSL Pinning - for API security
+        CertificatePinner certificatePinner = new CertificatePinner.Builder()
+                .add("www.qm.org.qa", "sha256/k+i4Cxf8v2rkPvB5i3UnjN7PnxZIOmU81OBeFR29db0=")
+                .build();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .certificatePinner(certificatePinner)
+                .build();
+
+        return new Retrofit.Builder()
                 .baseUrl(apiBaseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
-
-        return retrofit;
     }
 }
