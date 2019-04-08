eb272c671bcebe4ffbde40e884ab8fdec9aaaf08
Sanoop c <sanoopchullian22@gmail.com>
2019-04-08 14:27:15 +0530

    SSL pinning for older versions

--- app/src/main/java/com/qatarmuseums/qatarmuseumsapp/apicall/APIClient.java
+++ app/src/main/java/com/qatarmuseums/qatarmuseumsapp/apicall/APIClient.java
@@ -1,32 +1,68 @@
 package com.qatarmuseums.qatarmuseumsapp.apicall;
 
+import android.util.Log;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+
 import okhttp3.CertificatePinner;
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
+    private static final String TAG = "APIClient";
     public static String apiBaseUrl = "https://www.qm.org.qa/";
+    private static final String HOST = "qm.org.qa";
+    private static final String PUBLIC_KEY_HASH = "sha256/wiI3qA5+usYoQ7MpluMNIqvfN+EdSv7GxO5ZnQTOflQ=";
+    private static Retrofit retrofit;
 
     public static Retrofit getClient() {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        if (retrofit == null) {
+            OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder();
 
-        // SSL Pinning - for API security
-        CertificatePinner certificatePinner = new CertificatePinner.Builder()
-                .add("www.qm.org.qa", "sha256/k+i4Cxf8v2rkPvB5i3UnjN7PnxZIOmU81OBeFR29db0=")
-                .build();
+            // Enable the Network Logs
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .certificatePinner(certificatePinner)
-                .build();
+            // SSL Pinning - for API security
+            CertificatePinner certificatePinner = new CertificatePinner.Builder()
+                    .add(HOST, PUBLIC_KEY_HASH)
+                    .build();
 
-        return new Retrofit.Builder()
-                .baseUrl(apiBaseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build();
+            // Force the connection to use only TLS v.1.2 avoiding the fallback to older version to avoid vulnerabilities
+            final ConnectionSpec.Builder connectionSpec =
+                    new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS);
+            connectionSpec.tlsVersions(TlsVersion.TLS_1_2).build();
+
+            // Enable TLS specific version V.1.2
+            TLSSocketFactory tlsSocketFactory;
+
+            try {
+                tlsSocketFactory = new TLSSocketFactory();
+                httpBuilder.sslSocketFactory(tlsSocketFactory, tlsSocketFactory.systemDefaultTrustManager());
+            } catch (KeyManagementException | NoSuchAlgorithmException e) {
+                Log.d(TAG, "Failed to create Socket connection ");
+                e.printStackTrace();
+            }
+
+
+            OkHttpClient client = httpBuilder
+                    .addInterceptor(interceptor)
+                    .certificatePinner(certificatePinner)
+                    .connectionSpecs(Collections.singletonList(connectionSpec.build()))
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(apiBaseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit;
     }
 }
